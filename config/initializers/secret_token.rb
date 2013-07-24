730470a26433562b19c545069af19a24c753a5ab
Charles Massry <charlesdmassry@gmail.com>
2013-07-24 17:29:01 -0400

    Created rails application, installed twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BountyHunterNetwork::Application.config.secret_token = '260287a63f7b07579aa28c3143333009d0bfbe83c8d7a008ae96e42a1b89c9f4511c5c6b5c88b752930b13a990bc82a0a1e09974525feb7844d57733254f3e42'
